b28022a2acb7d328d44e9c0ead431fca089a20ef
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2017-01-09 12:00:26 +0800

    add mongodb module and save file path to db

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ let http  = require('http');
 let fs = require('fs');
 let express = require('express');
 
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/web_dev');
+mongoose.connection.on('connected', function() {
+    console.log('mongo db connected.');
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('mongo db connect failed with error: ' + err + '.');
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('mongo db disconnected.');
+});
+
 let app = express();
 let server = http.createServer(app);
 
+let bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 let ddlog = require('./utils/ddlog');
+let upload = require('./utils/upload');
+
+let HotFix = require('./db/hotfix');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -14,9 +34,6 @@ app.set('views', __dirname + '/views');
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-  // fs.readFile('./index.html', 'utf-8', function(err, data) {
-  //   res.end(data);
-  // });
   res.render('index.ejs');
 });
 
@@ -26,6 +43,17 @@ app.get('/favicon.ico', function(req, res) {
     res.write(data, 'binary');
     res.end();
   });
+});
+
+app.post('/upload-image', upload.hotfix.single('image'), function(req, res) {
+  let hotfix = new HotFix();
+  hotfix.name = req.file.filename;
+  hotfix.mime = req.file.mimetype;
+  hotfix.path = req.file.path;
+  hotfix.size = req.file.size;
+
+  hotfix.save();
+  res.send({success: true});
 });
 
 server.listen(3000, function() {
