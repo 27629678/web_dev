3913d44f871ac79f9d0c7426dfae7eb19c22d3aa
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2017-01-09 14:07:24 +0800

    add comment for add instance method and static method

--- db/hotfix.js
+++ db/hotfix.js
@@ -8,15 +8,21 @@
     path: String,
   });
 
-  // add method to instance of collection items(document)
-  schema.methods.fileName = function() {
+  // Adds an instance method to documents constructed from Models compiled from this schema.
+  // schema.methods.fileName = function() {
+  //   return this.name;
+  // }
+  schema.method('fileName', function() {
     return this.name;
-  }
+  });
 
-  // add method to model
-  schema.statics.findAll = function(cb) {
+  // Adds static "class" methods to Models compiled from this schema.
+  // schema.statics.findAll = function(cb) {
+  //   return this.find({}, cb);
+  // }
+  schema.static('findAll', function(cb) {
     return this.find({}, cb);
-  }
+  });
 
   module.exports = mongoose.model('file', schema);
 })();
