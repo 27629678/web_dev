616bea962edbfd7278b5743400162a8e05d5a63f
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2017-01-16 19:10:59 +0800

    add websocket entry

---
+++ client/Demo/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by hzyuxiaohua on 2017/1/16.
+//  Copyright © 2017年 XY Network Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    private let cellIdentifier = "demo-list-cell-identifier"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Demo Lists"
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    // MARK: - table view delegate and data source methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "websocket"
+        
+        return cell
+    }
+}
+
